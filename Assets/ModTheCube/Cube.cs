c10ae9d32c4ed6e99171ff68d6bc478be5b8dcd5
LeveledRogue <shanerickettswork@gmail.com>
2021-11-11 21:51:51 -0500

    Second Commit

---
+++ Assets/ModTheCube/Cube.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    private float rotationSpeed;
+    private float time = 0.0f;
+    public float cooldown = 1f;
+    
+    void Start()
+    {
+        ChangeCubeValues();
+    }
+
+    void Update()
+    {
+        transform.Rotate(rotationSpeed, rotationSpeed, rotationSpeed);
+        
+        time += Time.deltaTime;
+        if (time >= cooldown)
+        {
+            time = time - cooldown;
+            ChangeCubeValues();
+        }
+    }
+
+    void ChangeCubeValues()
+    {
+        rotationSpeed = Random.Range(-50, 50) * Time.deltaTime;
+        transform.position = Vector3.zero + new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), Random.Range(-5, 5));
+        transform.localScale = new Vector3(Random.Range(1, 5), Random.Range(1, 5), Random.Range(1, 5));
+        Material material = Renderer.material;
+        material.color = Random.ColorHSV(0, 1, 0, 1, 0, 1, 0, 1);
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Mod the Cube
+
+Did the mod the cub challenge and added random color swaps, random movements, and random scale changes via script that are triggered by a 1 second timer. This project is not interactable.
+
+[Test it out!](https://play.unity.com/mg/other/webgl-builds-116751)
